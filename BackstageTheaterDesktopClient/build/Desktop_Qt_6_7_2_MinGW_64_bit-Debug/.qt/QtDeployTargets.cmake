1d4a028871a45f3bc6c99860017c08b908935ce3
SimpleFreak <ISFreak@yandex.ru>
2024-10-28 16:25:05 +0500

    Добавил папку build Desktop приложения.

---
+++ BackstageTheaterDesktopClient/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_BackstageTheaterDesktopClient_FILE D:/Programming/BackstageTheater/BackstageTheaterDesktopClient/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/BackstageTheaterDesktopClient.exe)
+set(__QT_DEPLOY_TARGET_BackstageTheaterDesktopClient_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_BackstageTheaterDesktopClient_RUNTIME_DLLS D:/QT/6.7.2/mingw_64/bin/Qt6Widgets.dll;D:/QT/6.7.2/mingw_64/bin/Qt6Gui.dll;D:/QT/6.7.2/mingw_64/bin/Qt6Core.dll)
